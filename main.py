727e0932f193aa83e751e8912005a219ef9079f0
Jumagul <kultaevajumagul@gmail.com>
2025-05-15 15:44:20 +0600

    12-сабак: Класстар жана объекттер

---
+++ main.py
@@ -0,0 +1,5 @@
+from person import Person
+
+# Объект түзөбүз
+student1 = Person("Жумагүл", 25)
+student1.greet()
---
+++ person.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Салам! Менин атым {self.name}, жашым {self.age}.")
